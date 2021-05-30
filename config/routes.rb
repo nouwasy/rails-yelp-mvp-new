3973e68ede7c4509074f62743fbe1b6078c3f0e6
Cory Johnson <coryjohnson@protonmail.com>
2021-05-30 10:35:55 +0100

    almost completed challenge, just stuck on review.new

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:new, :index, :create, :show] do
+    resources :reviews, only: [:create, :new]
+  end
 end
