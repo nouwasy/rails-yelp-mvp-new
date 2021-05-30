00c7cdbc70c3063049d8db628338ec9626375640
Cory Johnson <coryjohnson@protonmail.com>
2021-05-30 16:52:35 +0100

    corrected numericality error, substituting inclusion and an array of 6 numbers [0, 1, 2, 3, 4, 5].

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-  validates :rating, numericality: { only_integer: true, in: 0..5 }
+  validates :rating, numericality: { only_integer: true }
   validates :content, :rating, presence: true
+  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5], allow_nil: false }
 end
